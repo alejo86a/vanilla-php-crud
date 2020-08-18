59f440dfbd1d72862a9368ffe99f809370e8271a
Alejandro Berrio Marin <alejo86a@gmail..com>
2020-08-18 09:42:15 -0500

    Front: create form to create account, logged in, search for messages and create message

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,7 @@
+document.getElementById("message-panel").style.display = "none"
+
+let usernameLoggedIn;
+
 document.addEventListener('click', function (event) {
     event.preventDefault();
     if (event.target.matches('#registration-btn')) {
@@ -14,7 +18,82 @@ document.addEventListener('click', function (event) {
         console.log(data,"data");
         Http.Post('/api/account', data)
             .then(() => {
-                console.log(data,"data");
+                usernameLoggedIn = data.Username;
+                document.getElementById("registrationForm").reset();
+                document.getElementById("account-panel").style.display = "none";
+                getAllMessages();
             })
     }
-}, false)
+}, false);
+document.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (event.target.matches('#login-btn')) {
+        var UsernameInput = document.getElementById('loginUsername');
+        var PasswordInput = document.getElementById('loginPassword');
+        var data = {
+            Username: UsernameInput.value,
+            Password: PasswordInput.value
+        };
+        console.log(data,"data");
+        Http.Get('/api/account?Username='+data.Username+'&Password='+data.Password)
+            .then(() => {
+                usernameLoggedIn = data.Username;
+                document.getElementById("loginForm").reset();
+                document.getElementById("account-panel").style.display = "none";
+                getAllMessages();
+            })
+    }
+}, false);
+document.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (event.target.matches('#message-btn')) {
+        var commentInput = document.getElementById('commentMessage');
+        const date = new Date();
+        var data = {
+            Username: usernameLoggedIn,
+            Comment: commentInput.value,
+            Created_at: date.getFullYear()+"-"+date.getUTCDay()+"-"+date.getUTCDate();
+        };
+        console.log(data,"data");
+        Http.Post('/api/message', data)
+            .then(() => {
+                document.getElementById("messageForm").reset();
+                document.getElementById("account-panel").style.display = "none";
+                getAllMessages();
+            })
+    }
+}, false);
+document.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (event.target.matches('#search-btn')) {
+        var searchMessageInput = document.getElementById('searchMessage');
+        var dateMessageInput = document.getElementById('dateMessage');
+        const query = dateMessageInput.value!=="" ? "date="+dateMessageInput.value : "search="+searchMessageInput.value; 
+        Http.Get('/api/message?'+query)
+            .then(response=> {                
+                listMessages(response);
+                document.getElementById("searchForm").reset();
+                document.getElementById("account-panel").style.display = "none";
+                getAllMessages();
+            })
+    }
+}, false);
+function getAllMessages() {
+    Http.Get('/api/message')
+        .then(response => {
+            listMessages(response);
+            document.getElementById("message-panel").style.display = "block";
+        })
+}
+
+function listMessages(response) {
+    const element = document.getElementById("messages-list");
+    const d_nested = document.getElementById("nested");
+    if(d_nested && d_nested.parentNode){
+        d_nested.parentNode.removeChild(d_nested);
+    }
+    response.json().then(data=>{
+        data.forEach(m=>element.insertAdjacentHTML("afterend", 
+            "<li id='nested'><h4>"+m.Created_at+"</h4><p>"+m.Comment+"</p><span>By: "+m.Username+"</span></li>"))    
+    });
+}
